ef7337debc396e8a26682d5dc984a9e6bd713d65
shivraj32644 <shivraj32644@gmail.com>
2024-05-19 20:22:26 +0530

    project initaited

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className='flex h-screen w-full  items-center justify-center'>
+      <h1 className="text-[80px]">Hello React App</h1>
+    </div>
+  )
+}
+
+export default App

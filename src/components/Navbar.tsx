8d71081b58a4c471ba41d88cf8ced5dfaebbb73d
shivraj32644 <shivraj32644@gmail.com>
2024-05-20 16:03:46 +0530

    feat: add NavBar component in application

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import ThemeToggle from "./ThemeToggle"
+
+const Navbar = () => {
+  return (
+    <div className="bg-[var(--background)] flex items-center py-2 px-4  w-full shadow-md">
+        <div className="flex-1">
+
+        </div>
+         <ThemeToggle />
+    </div>
+  )
+}
+
+export default Navbar
